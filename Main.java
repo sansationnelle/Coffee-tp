8302eab036e03afae456d927b107ecac0ca60272
Shap <sansationnelle01@gmail.com>
2025-12-16 09:58:43 +0100

    recharge de la machine à max pour le respect de la consigne

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Coffee machine = new Coffee(50,20,5,0,0,0);
+        Coffee machine = new Coffee(100,50,10,0,0,0);
         while (true){
             machine.menu();
         }
